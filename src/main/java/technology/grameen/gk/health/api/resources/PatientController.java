6c49efd2a8078c6e1d6e189b634d5bfc650828d4
muhimel <mu.himel@gmail.com>
2021-03-22 16:12:38 +0600

    'some-update'

--- src/main/java/technology/grameen/gk/health/api/resources/PatientController.java
+++ src/main/java/technology/grameen/gk/health/api/resources/PatientController.java
@@ -7,15 +7,14 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import technology.grameen.gk.health.api.entity.CardMember;
 import technology.grameen.gk.health.api.entity.CardRegistration;
 import technology.grameen.gk.health.api.entity.Patient;
 import technology.grameen.gk.health.api.projection.PatientNumberAutoComplete;
 import technology.grameen.gk.health.api.requests.PatientRequest;
-import technology.grameen.gk.health.api.responses.EntityCollectionResponse;
-import technology.grameen.gk.health.api.responses.ExceptionResponse;
-import technology.grameen.gk.health.api.responses.IResponse;
-import technology.grameen.gk.health.api.responses.PatientCreationResponse;
+import technology.grameen.gk.health.api.responses.*;
 import technology.grameen.gk.health.api.services.PatientManageService;
+import technology.grameen.gk.health.api.services.card_registration.CardMemberService;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,9 +24,11 @@ import java.util.Optional;
 public class PatientController {
 
     private PatientManageService patientManageService;
+    private CardMemberService cardMemberService;
 
-    PatientController(PatientManageService patientManageService){
+    PatientController(PatientManageService patientManageService, CardMemberService cardMemberService){
        this.patientManageService = patientManageService;
+       this.cardMemberService = cardMemberService;
     }
 
 
@@ -55,6 +56,20 @@ public class PatientController {
         try {
             Patient newPatient = patientManageService.addPatient(patient);
             return new ResponseEntity<>(new PatientCreationResponse(HttpStatus.OK.value(),newPatient), HttpStatus.OK);
+        }catch (Exception ex){
+            return new ResponseEntity<>(new PatientCreationResponse(HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                    ex.getMessage()),HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+    }
+
+    @PostMapping(value = "/add/from-member")
+    public ResponseEntity<IResponse>addPatientFromMember(@RequestBody CardMember cardMember){
+        try {
+
+            Patient newPatient = patientManageService.addPatient(new PatientRequest(cardMember));
+            cardMemberService.updateMember(cardMember);
+            return new ResponseEntity<>(new EntityResponse<>(HttpStatus.OK.value(),cardMember), HttpStatus.OK);
         }catch (Exception ex){
             return new ResponseEntity<>(new PatientCreationResponse(HttpStatus.UNPROCESSABLE_ENTITY.value(),
                     ex.getMessage()),HttpStatus.UNPROCESSABLE_ENTITY);
