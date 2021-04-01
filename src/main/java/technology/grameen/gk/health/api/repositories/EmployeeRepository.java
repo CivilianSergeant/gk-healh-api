ef3ca642d4b2c12c72b4b8f97ff1ffa12c4e3c9f
momin <islam.shaiful7@gmail.com>
2021-04-01 10:39:15 +0600

    updated

--- src/main/java/technology/grameen/gk/health/api/repositories/EmployeeRepository.java
+++ src/main/java/technology/grameen/gk/health/api/repositories/EmployeeRepository.java
@@ -2,10 +2,12 @@ package technology.grameen.gk.health.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import technology.grameen.gk.health.api.entity.Employee;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
@@ -13,5 +15,6 @@ public interface EmployeeRepository extends JpaRepository<Employee,Long> {
     @Query("Select e from Employee e JOIN FETCH e.center c ")
     public List<Employee> findAll();
 
-    Employee findByApiEmployeeId(Long id);
+    @Query("Select e from Employee e JOIN FETCH e.center c WHERE  e.apiEmployeeId=:id")
+    Optional<Employee> findByApiEmployeeId(@Param("id") Long id);
 }
